69677a6258dca46d35a1442caf8511ccba71a0c3
yuanjuan <yuanjuan@yuanjuandeMacBook-Pro.local>
2024-02-06 18:22:04 +0800

    chore: 整合Element Plus

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -5,8 +5,13 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 export {}
 
-declare module "vue" {
+declare module 'vue' {
   export interface GlobalComponents {
-    HelloWorld: (typeof import("./../components/HelloWorld.vue"))["default"];
+    ElButton: typeof import('element-plus/es')['ElButton']
+    HelloWorld: typeof import('./../components/HelloWorld.vue')['default']
+    IEpInfoFilled: typeof import('~icons/ep/info-filled')['default']
+    IEpSuccessFilled: typeof import('~icons/ep/success-filled')['default']
+    IEpWarningFilled: typeof import('~icons/ep/warning-filled')['default']
+    IEpWarnTriangleFilled: typeof import('~icons/ep/warn-triangle-filled')['default']
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { UserConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
 
 const pathSrc = path.resolve(__dirname, "src");
 
@@ -14,6 +17,7 @@ export default (): UserConfig => {
       alias: {
         "@": pathSrc,
       },
+      dedupe: ["vue"],
     },
     plugins: [
       vue(),
@@ -24,10 +28,29 @@ export default (): UserConfig => {
           enabled: true, // 是否自动生成 eslint 规则，建议生成之后设置 false
           filepath: "./.eslintrc-auto-import.json", // 指定自动导入函数 eslint 规则的文件
         },
+        resolvers: [
+          // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+          ElementPlusResolver(),
+          // 自动导入图标组件
+          IconsResolver({}),
+        ],
+        vueTemplate: true, // 是否在 vue 模板中自动导入
         dts: path.resolve(pathSrc, "types", "auto-imports.d.ts"), // 指定自动导入函数TS类型声明文件路径
       }),
       Components({
+        resolvers: [
+          // 自动导入 Element Plus 组件
+          ElementPlusResolver(),
+          // 自动注册图标组件
+          IconsResolver({
+            enabledCollections: ["ep"], // element-plus图标库，其他图标库 https://icon-sets.iconify.design/
+          }),
+        ],
         dts: path.resolve(pathSrc, "types", "components.d.ts"), // 指定自动导入组件TS类型声明文件路径
+      }),
+      Icons({
+        // 自动安装图标库
+        autoInstall: true,
       }),
     ],
   };
