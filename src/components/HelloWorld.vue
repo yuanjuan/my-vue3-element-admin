badc3b49d89739485c579a8e23a7f103fe41b49a
yuanjuan <yuanjuan@yuanjuandeMacBook-Pro.local>
2024-02-07 11:25:53 +0800

    chore: 整合 SCSS

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import variables from "@/styles/variables.module.scss";
 
 defineProps<{ msg: string }>();
 
@@ -8,7 +9,7 @@ const count = ref(0);
 
 <template>
   <h1>{{ msg }}</h1>
-  <div class="card">
+  <div class="card box">
     <button type="button" @click="count++">count is {{ count }}</button>
   </div>
   <div>
@@ -18,10 +19,17 @@ const count = ref(0);
     <el-button type="danger"><i-ep-WarnTriangleFilled />Danger</el-button>
     <el-button type="info"><svg-icon icon-class="vue" />SVG 本地图标</el-button>
   </div>
+  <div style="width: 100px; height: 100px" :style="{ 'background-color': variables.bgColor }" />
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .read-the-docs {
   color: #888;
+}
+
+.box {
+  width: 100px;
+  height: 100px;
+  background-color: $bg-color;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -60,5 +60,15 @@ export default (): UserConfig => {
         symbolId: "icon-[dir]-[name]",
       }),
     ],
+    css: {
+      // CSS 预处理器
+      preprocessorOptions: {
+        //define global scss variable
+        scss: {
+          javascriptEnabled: true,
+          additionalData: `@use "@/styles/variables.scss" as *;`,
+        },
+      },
+    },
   };
 };
