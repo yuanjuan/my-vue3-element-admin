e764672914795f4f2ea97a9438bb6cb9c8001caa
yuanjuan <yuanjuan@yuanjuandeMacBook-Pro.local>
2024-02-29 19:27:34 +0800

    feat: :sparkles: 添加请求进度条-NProgress

--- src/permission.ts
+++ src/permission.ts
@@ -1,14 +1,21 @@
 import router from "@/router/index";
+
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+NProgress.configure({ showSpinner: false }); // 进度条
 // 白名单路由
 const whiteList = ["/login"];
 
 router.beforeEach((to, from, next) => {
+  NProgress.start();
   const hasToken = localStorage.getItem("accessToken");
   // 如果hasToken存在，即代表已经登陆过了
   if (hasToken) {
     if (to.path === "/login") {
       // 如果已登录，跳转首页
       next("/");
+      NProgress.done();
     } else {
       next();
     }
@@ -18,6 +25,11 @@ router.beforeEach((to, from, next) => {
       next();
     } else {
       next(`/login?redirect=${to.path}`);
+      NProgress.done();
     }
   }
+});
+
+router.afterEach(() => {
+  NProgress.done();
 });
